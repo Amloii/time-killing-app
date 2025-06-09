323bf457c0483cce054a4b0d1ea140573a58952c
Daniel Gomez <36378037+Amloii@users.noreply.github.com>
2025-06-09 12:18:46 +0200

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -100,7 +100,7 @@ const Dashboard: React.FC = () => {
         {/* Show mascot on tasks page */}
         {activeTab === 'tasks' && (
           <div className="flex justify-center">
-            <SamuraiMascot mood="ready" size={100} />
+            <SamuraiMascot mood="ready\" size={100} />
           </div>
         )}
         
