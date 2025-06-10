49c44394329ad29638ea97c803c7ece6e553cf97
Daniel Gomez <36378037+Amloii@users.noreply.github.com>
2025-06-10 10:43:33 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App() {
           <Layout>
             <Routes>
               <Route path="/" element={<Navigate to="/dashboard/battle\" replace />} />
-              <Route path="/dashboard/*" element={<Dashboard />} />
+              <Route path="/dashboard/*\" element={<Dashboard />} />
               <Route path="/task/:taskId" element={<TaskDetails />} />
               <Route path="/rewards" element={<Rewards />} />
               <Route path="/statistics" element={<Statistics />} />
